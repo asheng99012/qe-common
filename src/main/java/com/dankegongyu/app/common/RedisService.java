e527ac26d0f37700d34c68b0880f2ce3b9b9cc0f
asheng <zhengjiansheng@dankegongyu.com>
2018-02-11 09:56:35 +0800

    修改禁用redis的方法

--- src/main/java/com/dankegongyu/app/common/RedisService.java
+++ src/main/java/com/dankegongyu/app/common/RedisService.java
@@ -2,6 +2,7 @@ package com.dankegongyu.app.common;
 
 import com.google.common.base.Function;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
@@ -14,23 +15,23 @@ import java.util.concurrent.TimeUnit;
 @Service()
 public class RedisService {
 
-    @Autowired
-    SystemConfig systemConfig;
-
     @Autowired(required = false)
     private RedisTemplate redisTemplate;
+
+    @Value("${redis.disable:false}")
+    private Boolean disable;
 
     public RedisTemplate getRedisTemplate() {
         return redisTemplate;
     }
 
     public void set(String key, Object object) {
-        if (systemConfig.getEnv().equals(SystemConfig.ENV_DEV)) return;
+        if (disable) return;
         redisTemplate.opsForValue().set(key, object);
     }
 
     public void set(String key, Object object, int minute) {
-        if (systemConfig.getEnv().equals(SystemConfig.ENV_DEV)) return;
+        if (disable) return;
         redisTemplate.opsForValue().set(key, object, minute, TimeUnit.MINUTES);
     }
 
