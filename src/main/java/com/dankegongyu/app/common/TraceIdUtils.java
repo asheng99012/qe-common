3bc653db6bcc2c5bd9cd14a4f3918af10847f0d0
asheng <zhengjiansheng@dankegongyu.com>
2018-11-29 10:38:53 +0800

    添加trace 工具类

--- src/main/java/com/dankegongyu/app/common/TraceIdUtils.java
+++ src/main/java/com/dankegongyu/app/common/TraceIdUtils.java
@@ -1,4 +1,21 @@
 package com.dankegongyu.app.common;
 
+import com.dankegongyu.common.util.UUID19;
+import org.slf4j.MDC;
+
 public class TraceIdUtils {
+    static String traceId = "traceId";
+
+    public static void setTraceId(String traceId) {
+        MDC.put(traceId, traceId);
+    }
+
+    public static void setTraceId() {
+        if (getTraceId() == null)
+            setTraceId(UUID19.randomUUID());
+    }
+
+    public static String getTraceId() {
+        return MDC.get(traceId);
+    }
 }
