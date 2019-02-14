acdf835cde1c7a5a25577d1bdf496fdffbd96907
asheng <zhengjiansheng@dankegongyu.com>
2019-02-14 12:19:32 +0800

    evnt bus

--- src/main/java/com/dankegongyu/app/common/event/EventBus.java
+++ src/main/java/com/dankegongyu/app/common/event/EventBus.java
@@ -1,4 +1,27 @@
 package com.dankegongyu.app.common.event;
 
-public class EventBus {
+import com.dankegongyu.app.common.AppUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public abstract class EventBus<E extends ApplicationEvent> implements ApplicationListener {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    private boolean isStart = false;
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (AppUtils.getApplicationContext().getParent() == null) {
+            if (!isStart) {
+                logger.info("开始处理事件:{}", event.getClass().getName());
+                isStart = true;
+                process((E) event);
+            } else {
+                logger.info("重复执行事:{}", event.getClass().getName());
+            }
+        }
+    }
+
+    public abstract void process(E event);
 }
