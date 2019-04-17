a5920468cd1ddf8f2e44ef1c947394e27aca575f
asheng <zhengjiansheng@dankegongyu.com>
2019-04-17 15:31:42 +0800

    增加log

--- src/main/java/com/dankegongyu/app/common/mq/CommonListener.java
+++ src/main/java/com/dankegongyu/app/common/mq/CommonListener.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.dankegongyu.app.common.RpcService;
 import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
  * 配合 proxy 使用的异步任务处理
  */
 public class CommonListener extends BaseListener {
+    Logger logger = LoggerFactory.getLogger(CommonListener.class);
     @Autowired
     RpcService rpcService;
 
@@ -21,6 +24,7 @@ public class CommonListener extends BaseListener {
         try {
             rpcService.run(new Object[]{data.get("class"), data.get("method")}, JSON.parseArray(data.get("data").toString()).toArray());
         } catch (Exception e) {
+            logger.error(e.getMessage(),e.getCause());
             throw new RuntimeException(e.getMessage(), e.getCause());
         }
     }
