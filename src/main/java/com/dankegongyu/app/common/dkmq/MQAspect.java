5b3220068cbceae20848cff784b0a94dd6368736
郑建生 <zhengjiansheng@danke.com>
2019-11-04 18:43:45 +0800

    清空id

--- src/main/java/com/dankegongyu/app/common/dkmq/MQAspect.java
+++ src/main/java/com/dankegongyu/app/common/dkmq/MQAspect.java
@@ -47,6 +47,7 @@ public class MQAspect {
             throw new BusinessException(JsonUtils.toJson(msg) + ":" + t.getMessage(), t.getCause());
         } finally {
             log.info("发送数据：" + JsonUtils.toJson(msg));
+            TraceIdUtils.clear();
             if (mqlog != null) {
                 mqlog.log(key, msg.getTopic(), msg.getTag(), msg.getBornHost(),
                         msg.getPayload(), msg, 0, "com.danke.infra.mq.common.producer.MessageTemplate",
@@ -87,6 +88,7 @@ public class MQAspect {
             throw new BusinessException(JsonUtils.toJson(msg) + ":" + t.getMessage(), t.getCause());
         } finally {
             log.info("消费数据：" + JsonUtils.toJson(msg) + ",结果：" + errmsg);
+            TraceIdUtils.clear();
             if (mqlog != null) {
 
                 mqlog.log(msg.getMsgId(), msg.getTopic(), msg.getTag(),
