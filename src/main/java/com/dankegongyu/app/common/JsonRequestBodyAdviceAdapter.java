1f5ef40526822d95c3ad8b5fd24ffc204eaf8aa6
郑建生 <zhengjiansheng@danke.com>
2020-01-10 20:40:19 +0800

    bugfix

--- src/main/java/com/dankegongyu/app/common/JsonRequestBodyAdviceAdapter.java
+++ src/main/java/com/dankegongyu/app/common/JsonRequestBodyAdviceAdapter.java
@@ -8,7 +8,9 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
 
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
@@ -25,9 +27,17 @@ public class JsonRequestBodyAdviceAdapter extends RequestBodyAdviceAdapter {
                                   MethodParameter parameter, Type targetType,
                                   Class<? extends HttpMessageConverter<?>> converterType) {
         Map data = FormFilter.getParameters();
-        if (targetType.getTypeName().equals("java.lang.Object[]") && data.containsKey("data"))
+        if (isArray(targetType) && data.containsKey("data"))
             return JsonUtils.convert(data.get("data"), targetType);
         else
             return JsonUtils.convert(data, targetType);
+    }
+
+    public boolean isArray(Type targetType) {
+        if (targetType.getTypeName().equals("java.lang.Object[]"))
+            return true;
+        if (targetType instanceof ParameterizedType && ((ParameterizedType) targetType).getRawType() == List.class)
+            return true;
+        return false;
     }
 }
